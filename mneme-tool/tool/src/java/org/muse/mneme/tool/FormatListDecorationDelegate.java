4d741f330e2baed86845c2198f1c4d12bab4f0ad
ggolden@umich.edu <ggolden@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-05-22 22:56:45 +0000

    start of list view extraction
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@9621 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/FormatListDecorationDelegate.java
@@ -0,0 +1,123 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007 The Regents of the University of Michigan & Foothill College, ETUDES Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.muse.mneme.tool;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.util.FormatDelegateImpl;
+import org.muse.mneme.api.AssessmentService;
+import org.muse.mneme.api.AssessmentSubmissionStatus;
+import org.muse.mneme.api.Submission;
+
+/**
+ * The "FormatListDecoration" format delegate for the mneme tool.
+ */
+public class FormatListDecorationDelegate extends FormatDelegateImpl
+{
+	/** Our log. */
+	private static Log M_log = LogFactory.getLog(FormatListDecorationDelegate.class);
+
+	/**
+	 * Shutdown.
+	 */
+	public void destroy()
+	{
+		M_log.info("destroy()");
+	}
+
+	/**
+	 * Final initialization, once all dependencies are set.
+	 */
+	public void init()
+	{
+		super.init();
+		M_log.info("init()");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String format(Context context, Object value)
+	{
+		Object o = context.get("submission");
+		if (!(o instanceof Submission)) return value.toString();
+		Submission submission = (Submission) o;
+
+		AssessmentSubmissionStatus status = submission.getAssessmentSubmissionStatus();
+
+		switch (status)
+		{
+			case future:
+			{
+				return "<img src=\"" + context.get("sakai.return.url") + "/icons/future.gif\" alt=\""
+						+ context.getMessages().getString("list-key-future") + "\" /><br /><span style=\"font-size:smaller\">"
+						+ context.getMessages().getString("list-key-future") + "</span>";
+			}
+
+			case ready:
+			{
+				return "<img src=\"" + context.get("sakai.return.url") + "/icons/begin.gif\" alt=\""
+						+ context.getMessages().getString("list-key-todo") + "\" /><br /><span style=\"font-size:smaller\">"
+						+ context.getMessages().getString("list-key-todo") + "</span>";
+			}
+
+			case inProgressAlert:
+			{
+				return "<img src=\"" + context.get("sakai.return.url") + "/icons/exit.gif\" alt=\""
+						+ context.getMessages().getString("list-key-inprogress") + "\" />" + "<img src=\"" + context.get("sakai.return.url")
+						+ "/icons/warning.gif\" alt=\"" + context.getMessages().getString("list-key-urgent") + "\" />"
+						+ "<br /><span style=\"font-size:smaller\">" + context.getMessages().getString("list-key-inprogress-urgent") + "</span>";
+			}
+
+			case inProgress:
+			{
+				return "<img src=\"" + context.get("sakai.return.url") + "/icons/exit.gif\" alt=\""
+						+ context.getMessages().getString("list-key-inprogress") + "\" /><br /><span style=\"font-size:smaller\">"
+						+ context.getMessages().getString("list-key-inprogress") + "</span>";
+			}
+
+			case completeReady:
+			{
+				return "<img src=\"" + context.get("sakai.return.url") + "/icons/finish.gif\" alt=\""
+						+ context.getMessages().getString("list-key-complete") + "\" />" + "<img src=\"" + context.get("sakai.return.url")
+						+ "/icons/begin.gif\" alt=\"" + context.getMessages().getString("list-key-repeat") + "\" />"
+						+ "<br /><span style=\"font-size:smaller\">" + context.getMessages().getString("list-key-complete-repeat") + "</span>";
+			}
+
+			case complete:
+			{
+				return "<img src=\"" + context.get("sakai.return.url") + "/icons/finish.gif\" alt=\""
+						+ context.getMessages().getString("list-key-complete") + "\" /><br /><span style=\"font-size:smaller\">"
+						+ context.getMessages().getString("list-key-complete") + "</span>";
+			}
+			case over:
+			{
+				return "<img src=\"" + context.get("sakai.return.url") + "/icons/cancel.gif\" alt=\""
+						+ context.getMessages().getString("list-key-overdue") + "\" /><br /><span style=\"font-size:smaller\">"
+						+ context.getMessages().getString("list-key-overdue") + "</span>";
+			}
+		}
+
+		return null;
+	}
+}
---
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/ListView.java
@@ -0,0 +1,192 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007 The Regents of the University of Michigan & Foothill College, ETUDES Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.muse.mneme.tool;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.util.ViewImpl;
+import org.muse.mneme.api.AssessmentService;
+import org.sakaiproject.tool.api.ToolManager;
+import org.sakaiproject.util.Web;
+
+/**
+ * The /list view for the mneme tool.
+ */
+public class ListView extends ViewImpl
+{
+	/** Our log. */
+	private static Log M_log = LogFactory.getLog(ListView.class);
+
+	/** Assessment service. */
+	protected AssessmentService assessmentService = null;
+
+	/** tool manager reference. */
+	protected ToolManager toolManager = null;
+
+	/**
+	 * Shutdown.
+	 */
+	public void destroy()
+	{
+		M_log.info("destroy()");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void get(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		// 0 or 1 parameters
+		if ((params.length != 2) && (params.length != 3))
+		{
+			throw new IllegalArgumentException();
+		}
+
+		// optional sort parameter
+		String sortCode = null;
+		if (params.length == 3)
+		{
+			sortCode = params[2];
+		}
+
+		// check security
+		if (!assessmentService.allowListDeliveryAssessment(toolManager.getCurrentPlacement().getContext()).booleanValue())
+		{
+			// redirect to error
+			res.sendRedirect(res.encodeRedirectURL(Web.returnUrl(req, "/error/" + Errors.unauthorized)));
+			return;
+		}
+
+		// SORT: 0|1|2 A|D - 2 chars, column | direction
+		if ((sortCode != null) && (sortCode.length() != 2))
+		{
+			// redirect to error
+			res.sendRedirect(res.encodeRedirectURL(Web.returnUrl(req, "/error/" + Errors.invalid)));
+			return;
+		}
+
+		AssessmentService.GetUserContextSubmissionsSort sort = AssessmentService.GetUserContextSubmissionsSort.title_a;
+		if (sortCode != null)
+		{
+			context.put("sort_column", sortCode.charAt(0));
+			context.put("sort_direction", sortCode.charAt(1));
+
+			// 0 is title
+			if ((sortCode.charAt(0) == '0') && (sortCode.charAt(1) == 'A'))
+			{
+				sort = AssessmentService.GetUserContextSubmissionsSort.title_a;
+			}
+			else if ((sortCode.charAt(0) == '0') && (sortCode.charAt(1) == 'D'))
+			{
+				sort = AssessmentService.GetUserContextSubmissionsSort.title_d;
+			}
+
+			// 1 is status
+			else if ((sortCode.charAt(0) == '1') && (sortCode.charAt(1) == 'A'))
+			{
+				sort = AssessmentService.GetUserContextSubmissionsSort.status_a;
+			}
+			else if ((sortCode.charAt(0) == '1') && (sortCode.charAt(1) == 'D'))
+			{
+				sort = AssessmentService.GetUserContextSubmissionsSort.status_d;
+			}
+
+			// 2 is due date
+			else if ((sortCode.charAt(0) == '2') && (sortCode.charAt(1) == 'A'))
+			{
+				sort = AssessmentService.GetUserContextSubmissionsSort.dueDate_a;
+			}
+			else if ((sortCode.charAt(0) == '2') && (sortCode.charAt(1) == 'D'))
+			{
+				sort = AssessmentService.GetUserContextSubmissionsSort.dueDate_d;
+			}
+
+			else
+			{
+				// redirect to error
+				res.sendRedirect(res.encodeRedirectURL(Web.returnUrl(req, "/error/" + Errors.invalid)));
+				return;
+			}
+		}
+
+		// default sort: status descending
+		if (sortCode == null)
+		{
+			context.put("sort_column", '1');
+			context.put("sort_direction", 'D');
+			sort = AssessmentService.GetUserContextSubmissionsSort.status_d;
+		}
+
+		// collect information: submissions / assessments
+		List submissions = assessmentService.getUserContextSubmissions(toolManager.getCurrentPlacement().getContext(), null, sort);
+		context.put("submissions", submissions);
+
+		// render
+		uiService.render(ui, context);
+	}
+
+	/**
+	 * Final initialization, once all dependencies are set.
+	 */
+	public void init()
+	{
+		super.init();
+		M_log.info("init()");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void post(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		throw new IllegalArgumentException();
+	}
+
+	/**
+	 * Set the assessment service.
+	 * 
+	 * @param service
+	 *        The assessment service.
+	 */
+	public void setAssessmentService(AssessmentService service)
+	{
+		this.assessmentService = service;
+	}
+
+	/**
+	 * Set the tool manager.
+	 * 
+	 * @param manager
+	 *        The tool manager.
+	 */
+	public void setToolManager(ToolManager manager)
+	{
+		toolManager = manager;
+	}
+}
