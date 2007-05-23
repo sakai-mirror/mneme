7213c62abc461433846e606410b98526c910fa8a
ggolden@umich.edu <ggolden@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-05-23 15:21:46 +0000

    completed list view extraction
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@9630 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/FormatListGradeDelegate.java
@@ -0,0 +1,103 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007 The Regents of the University of Michigan & Foothill College, ETUDES Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.muse.mneme.tool;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.util.FormatDelegateImpl;
+import org.muse.mneme.api.AssessmentService;
+import org.muse.mneme.api.AssessmentSubmissionStatus;
+import org.muse.mneme.api.Submission;
+
+/**
+ * The "FormatListGrade" format delegate for the mneme tool.
+ */
+public class FormatListGradeDelegate extends FormatDelegateImpl
+{
+	/** Our log. */
+	private static Log M_log = LogFactory.getLog(FormatListGradeDelegate.class);
+
+	/**
+	 * Format a score to 2 decimal places, trimming ".0" if present.
+	 * 
+	 * @param score
+	 *        The score to format.
+	 * @return The formatted score
+	 */
+	protected static String formatScore(float score)
+	{
+		// round to a single place
+		String rv = Float.toString(Math.round(score * 100.0f) / 100.0f);
+
+		// get rid of ".00"
+		if (rv.endsWith(".00"))
+		{
+			rv = rv.substring(0, rv.length() - 3);
+		}
+
+		// get rid of ".0"
+		if (rv.endsWith(".0"))
+		{
+			rv = rv.substring(0, rv.length() - 2);
+		}
+
+		return rv;
+	}
+
+	/**
+	 * Shutdown.
+	 */
+	public void destroy()
+	{
+		M_log.info("destroy()");
+	}
+
+	public String format(Context context, Object value)
+	{
+		if (!(value instanceof Submission)) return context.getMessages().getString("dash");
+		Submission submission = (Submission) value;
+
+		if (submission.getIsComplete().booleanValue())
+		{
+			if (submission.getIsGraded().booleanValue())
+			{
+				return formatScore(submission.getTotalScore());
+			}
+			else
+			{
+				return context.getMessages().getString("list-not-graded");
+			}
+		}
+
+		return context.getMessages().getString("dash");
+	}
+
+	/**
+	 * Final initialization, once all dependencies are set.
+	 */
+	public void init()
+	{
+		super.init();
+		M_log.info("init()");
+	}
+}
