8ece0d8e49687316b122dbc660ea69a7edcfdb28
tannirumurthy@foothill.edu <tannirumurthy@foothill.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-07-16 21:13:42 +0000

    Delete Pool View
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@10633 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/DeletePoolView.java
@@ -0,0 +1,108 @@
+/**********************************************************************************
+ * $URL: $
+ * $Id: $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.muse.mneme.tool;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.api.Values;
+import org.muse.ambrosia.util.ControllerImpl;
+import org.muse.mneme.api.PoolService;
+import org.sakaiproject.util.Web;
+
+/**
+ * The pools delete view for the mneme tool.
+ */
+public class DeletePoolView extends ControllerImpl
+{
+	/** Our log. */
+	private static Log M_log = LogFactory.getLog(DeletePoolView.class);
+	
+	/** Pool Service*/
+	protected PoolService poolService = null;
+
+	/**
+	 * Final initialization, once all dependencies are set.
+	 */
+	public void init()
+	{
+		super.init();
+		M_log.info("init()");
+	}
+	
+	/**
+	 * Shutdown.
+	 */
+	public void destroy()
+	{
+		M_log.info("destroy()");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void get(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		if (M_log.isInfoEnabled()) M_log.info("In get....");
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void post(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		//for the checkboxes
+		Values values = this.uiService.newValues();
+		context.put("poolids", values);
+
+		// read form
+		String destination = this.uiService.decode(req, context);
+		
+		String[] selectedPoolIds = values.getValues();
+		
+		res.sendRedirect(res.encodeRedirectURL(Web.returnUrl(req, destination)));
+		
+	}
+
+	/**
+	 * @return the poolService
+	 */
+	public PoolService getPoolService()
+	{
+		return this.poolService;
+	}
+
+	/**
+	 * @param poolService the poolService to set
+	 */
+	public void setPoolService(PoolService poolService)
+	{
+		this.poolService = poolService;
+	}
+
+}
