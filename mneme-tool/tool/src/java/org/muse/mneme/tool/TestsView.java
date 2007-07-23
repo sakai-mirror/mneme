14807a430a78ac79f2ec6709116f81eff5c70f06
ggolden@umich.edu <ggolden@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-07-23 16:31:41 +0000

    roughing in tests view
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@10747 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/TestsView.java
@@ -0,0 +1,116 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007 The Regents of the University of Michigan & Foothill College, ETUDES Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.muse.mneme.tool;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.api.Values;
+import org.muse.ambrosia.util.ControllerImpl;
+import org.muse.mneme.api.Assessment;
+import org.muse.mneme.api.AssessmentService;
+import org.sakaiproject.tool.api.ToolManager;
+
+/**
+ * The /tests view for the mneme tool.
+ */
+public class TestsView extends ControllerImpl
+{
+	/** Our log. */
+	private static Log M_log = LogFactory.getLog(TestsView.class);
+
+	/** Assessment service. */
+	protected AssessmentService assessmentService = null;
+
+	/** tool manager reference. */
+	protected ToolManager toolManager = null;
+
+	/**
+	 * Shutdown.
+	 */
+	public void destroy()
+	{
+		M_log.info("destroy()");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void get(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		// collect the assessments in this context
+		List<Assessment> assessments = this.assessmentService.getContextAssessments(this.toolManager.getCurrentPlacement().getContext());
+		context.put("assessments", assessments);
+
+		// value holders for the selection checkboxes
+		Values values = this.uiService.newValues();
+		context.put("ids", values);
+
+		// render
+		uiService.render(ui, context);
+	}
+
+	/**
+	 * Final initialization, once all dependencies are set.
+	 */
+	public void init()
+	{
+		super.init();
+		M_log.info("init()");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void post(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		throw new IllegalArgumentException();
+	}
+
+	/**
+	 * Set the AssessmentService.
+	 * 
+	 * @param service
+	 *        The AssessmentService.
+	 */
+	public void setAssessmentService(AssessmentService service)
+	{
+		this.assessmentService = service;
+	}
+
+	/**
+	 * Set the tool manager.
+	 * 
+	 * @param manager
+	 *        The tool manager.
+	 */
+	public void setToolManager(ToolManager manager)
+	{
+		toolManager = manager;
+	}
+}
