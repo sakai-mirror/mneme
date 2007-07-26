a0e2e71e1d9828d38b0e8550bfae35788ae188c3
tannirumurthy@foothill.edu <tannirumurthy@foothill.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-07-26 18:35:51 +0000

    added pools add files
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@10832 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/PoolsAddView.java
@@ -0,0 +1,84 @@
+/**********************************************************************************
+ * $URL: $
+ * $Id: $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.muse.mneme.tool;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.util.ControllerImpl;
+import org.muse.mneme.api.PoolService;
+import org.sakaiproject.i18n.InternationalizedMessages;
+import org.sakaiproject.tool.api.ToolManager;
+
+/**
+ * 
+ */
+public class PoolsAddView extends ControllerImpl
+{
+	/** Our log. */
+	private static Log M_log = LogFactory.getLog(PoolsAddView.class);
+
+	/** Pool Service */
+	protected PoolService poolService = null;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void get(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		//render
+		uiService.render(ui, context);
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void post(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		throw new IllegalArgumentException();
+
+	}
+
+	/**
+	 * @return the poolService
+	 */
+	public PoolService getPoolService()
+	{
+		return this.poolService;
+	}
+
+	/**
+	 * @param poolService
+	 *        the poolService to set
+	 */
+	public void setPoolService(PoolService poolService)
+	{
+		this.poolService = poolService;
+	}
+
+}
