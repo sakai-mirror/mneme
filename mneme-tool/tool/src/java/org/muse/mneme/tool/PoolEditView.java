7c28b0d8f4400cbf5064de386b450f45685ece5f
tannirumurthy@foothill.edu <tannirumurthy@foothill.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-08-10 01:23:03 +0000

    added intial code for entiry list and nav action bar
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@11140 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- mneme-tool/tool/src/java/org/muse/mneme/tool/PoolEditView.java
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/PoolEditView.java
@@ -22,6 +22,7 @@
 package org.muse.mneme.tool;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,9 +30,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.api.Values;
 import org.muse.ambrosia.util.ControllerImpl;
 import org.muse.mneme.api.Pool;
 import org.muse.mneme.api.PoolService;
+import org.muse.mneme.api.Question;
+import org.muse.mneme.api.QuestionService;
+import org.sakaiproject.tool.api.SessionManager;
 
 /**
  * The pools delete view for the mneme tool.
@@ -44,17 +49,31 @@ public class PoolEditView extends ControllerImpl
 	/** Pool Service */
 	protected PoolService poolService = null;
 
+	/** Question Service */
+	protected QuestionService questionService = null;
+
+	/** Dependency: SessionManager */
+	protected SessionManager sessionManager = null;
+
 	/**
 	 * {@inheritDoc}
 	 */
 	public void get(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
 	{
 		if (params.length != 3) throw new IllegalArgumentException();
-		
-		//setup the model: the selected pool
+
+		// setup the model: the selected pool
 		Pool pool = this.poolService.getPool(params[2]);
 		context.put("pool", pool);
-		
+
+		// get questions -- passed userid as parameter as findQuestions is not fetching with out userid
+		List<Question> questions = questionService.findQuestions(sessionManager.getCurrentSessionUserId(), pool, QuestionService.FindQuestionsSort.type_a, null, null, null);
+		context.put("questions", questions);
+
+		// for the checkboxes
+		Values values = this.uiService.newValues();
+		context.put("questionids", values);
+
 		uiService.render(ui, context);
 	}
 
@@ -64,15 +83,34 @@ public class PoolEditView extends ControllerImpl
 	public void post(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
 	{
 		throw new IllegalArgumentException();
-		
+
 	}
 
 	/**
-	 * @param poolService the poolService to set
+	 * @param poolService
+	 *        the poolService to set
 	 */
 	public void setPoolService(PoolService poolService)
 	{
 		this.poolService = poolService;
+	}
+
+	/**
+	 * @param questionService
+	 *        the questionService to set
+	 */
+	public void setQuestionService(QuestionService questionService)
+	{
+		this.questionService = questionService;
+	}
+
+	/**
+	 * @param sessionManager
+	 *        the sessionManager to set
+	 */
+	public void setSessionManager(SessionManager sessionManager)
+	{
+		this.sessionManager = sessionManager;
 	}
 
 }
