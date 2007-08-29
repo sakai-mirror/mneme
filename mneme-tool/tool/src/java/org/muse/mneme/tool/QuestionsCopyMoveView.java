5df7357c468efaad9962441a2626a99e421a7c7b
tannirumurthy@foothill.edu <tannirumurthy@foothill.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-08-29 22:25:25 +0000

    updated for copy and move question
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@11657 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- mneme-tool/tool/src/java/org/muse/mneme/tool/QuestionsCopyMoveView.java
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/QuestionsCopyMoveView.java
@@ -30,11 +30,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.api.Value;
 import org.muse.ambrosia.util.ControllerImpl;
+import org.muse.mneme.api.AssessmentPermissionException;
 import org.muse.mneme.api.MnemeService;
 import org.muse.mneme.api.Pool;
 import org.muse.mneme.api.PoolService;
+import org.muse.mneme.api.Question;
+import org.muse.mneme.api.QuestionService;
 import org.sakaiproject.tool.api.ToolManager;
+import org.sakaiproject.util.Web;
 
 /**
  * The /question_copy view for the mneme tool.
@@ -49,6 +54,9 @@ public class QuestionsCopyMoveView extends ControllerImpl
 
 	/** Pool Service */
 	protected PoolService poolService = null;
+
+	/** Dependency: Question service. */
+	protected QuestionService questionService = null;
 
 	/** Dependency: ToolManager */
 	protected ToolManager toolManager = null;
@@ -76,8 +84,9 @@ public class QuestionsCopyMoveView extends ControllerImpl
 	public void get(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
 	{
 		if (params.length != 6) throw new IllegalArgumentException();
-		
-		//pools sort param is in params array at index 2
+		context.put("saveDestination", context.getDestination());
+
+		// pools sort param is in params array at index 2
 		context.put("poolsSortCode", params[2]);
 
 		// pools paging parameter - is in params array at index 3
@@ -85,14 +94,21 @@ public class QuestionsCopyMoveView extends ControllerImpl
 
 		if (path.startsWith("question_copy"))
 			context.put("headerText", messages.get("copy-header-text"));
-		else if (path.startsWith("question_move")) 
-			context.put("headerText", messages.get("move-header-text"));
-		
-		//pools id is in params array at index 4
+		else if (path.startsWith("question_move")) context.put("headerText", messages.get("move-header-text"));
+
+		// pools id is in params array at index 4
 		context.put("poolid", params[4]);
+		
+		Pool pool = this.poolService.getPool(params[4]);
 
 		List<Pool> pools = this.poolService.findPools(toolManager.getCurrentPlacement().getContext(), null, null, null, null, null);
+		pools.remove(pool);
 		context.put("pools", pools);
+
+		// for the selected pool
+		Value value = this.uiService.newValue();
+		context.put("selectedPoolId", value);
+
 		// render
 		uiService.render(ui, context);
 	}
@@ -102,7 +118,45 @@ public class QuestionsCopyMoveView extends ControllerImpl
 	 */
 	public void post(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
 	{
-		throw new IllegalArgumentException();
+		if (params.length != 6) throw new IllegalArgumentException();
+
+		// for the selected pool
+		Value value = this.uiService.newValue();
+		context.put("selectedPoolId", value);
+
+		// read form
+		String destination = this.uiService.decode(req, context);
+
+		String selectedPoolId = value.getValue();
+
+		Pool pool = this.poolService.getPool(selectedPoolId);
+
+		Question question = null;
+
+		try
+		{
+			// question id's are in the params array at the index 5
+			String questionIds[] = params[5].split("\\+");
+
+			for (String questionId : questionIds)
+			{
+				// get the question
+				question = this.questionService.getQuestion(questionId);
+				if (path.startsWith("question_copy"))
+					this.questionService.copyQuestion(toolManager.getCurrentPlacement().getContext(), null, pool, question);
+				/*else if (path.startsWith("question_move"))
+					this.questionService.moveQuestion(toolManager.getCurrentPlacement().getContext(), null, pool, question);*/
+			}
+		}
+		catch (AssessmentPermissionException e)
+		{
+			// redirect to error
+			res.sendRedirect(res.encodeRedirectURL(Web.returnUrl(req, "/error/" + Errors.unauthorized)));
+			return;
+		}
+		if (params.length > 5) destination = "/pool_edit/" + params[2] + "/" + params[3] + "/" + params[4];
+
+		res.sendRedirect(res.encodeRedirectURL(Web.returnUrl(req, destination)));
 	}
 
 	/**
@@ -130,6 +184,15 @@ public class QuestionsCopyMoveView extends ControllerImpl
 	public void setToolManager(ToolManager toolManager)
 	{
 		this.toolManager = toolManager;
+	}
+
+	/**
+	 * @param questionService
+	 *        the questionService to set
+	 */
+	public void setQuestionService(QuestionService questionService)
+	{
+		this.questionService = questionService;
 	}
 
 }
