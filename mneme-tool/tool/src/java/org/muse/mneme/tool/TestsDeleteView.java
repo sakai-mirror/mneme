98404880c052c6a5e97fea33598cd08b018da318
ggolden@umich.edu <ggolden@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-09-06 13:38:45 +0000

    fixed copyright
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@11770 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- mneme-tool/tool/src/java/org/muse/mneme/tool/TestsDeleteView.java
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/TestsDeleteView.java
@@ -1,20 +1,20 @@
 /**********************************************************************************
- * $URL: https://source.sakaiproject.org/contrib/muse/mneme/trunk/mneme-tool/tool/src/java/org/muse/mneme/tool/TestsDeleteView.java $
- * $Id: TestsDeleteView.java 10793 2007-07-25 00:08:59Z tannirumurthy@foothill.edu $
+ * $URL: https://source.sakaiproject.org/contrib/muse/ambrosia/trunk/ambrosia-impl/impl/src/java/org/muse/ambrosia/impl/UiValue.java $
+ * $Id: UiValue.java 9047 2007-04-13 00:21:17Z ggolden@umich.edu $
  ***********************************************************************************
  *
- * Copyright (c) 2007 The Sakai Foundation.
+ * Copyright (c) 2007 The Regents of the University of Michigan & Foothill College, ETUDES Project
  * 
- * Licensed under the Educational Community License, Version 1.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *      http://www.opensource.org/licenses/ecl1.php
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  **********************************************************************************/
