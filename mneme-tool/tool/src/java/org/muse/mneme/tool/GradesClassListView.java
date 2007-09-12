777fdff5638fdafa90148f8214f280e8de556929
tannirumurthy@foothill.edu <tannirumurthy@foothill.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-09-12 16:30:28 +0000

    files for grade by submission added
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@11953 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ mneme-tool/tool/src/java/org/muse/mneme/tool/GradesClassListView.java
@@ -0,0 +1,112 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007 The Regents of the University of Michigan & Foothill College, ETUDES Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.muse.mneme.tool;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.muse.ambrosia.api.Context;
+import org.muse.ambrosia.util.ControllerImpl;
+import org.muse.mneme.api.Assessment;
+import org.muse.mneme.api.AssessmentService;
+import org.muse.mneme.api.SubmissionService;
+
+/**
+ * The /grading view for the mneme tool.
+ */
+public class GradesClassListView extends ControllerImpl
+{
+	/** Our log. */
+	private static Log M_log = LogFactory.getLog(GradesClassListView.class);
+
+	/** Assessment service. */
+	protected AssessmentService assessmentService = null;
+
+	/** Submission Service */
+	protected SubmissionService submissionService = null;
+
+	/**
+	 * Shutdown.
+	 */
+	public void destroy()
+	{
+		M_log.info("destroy()");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void get(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		if (params.length != 4) throw new IllegalArgumentException();
+
+		// get Assessment - assessment id is in params at index 3
+		Assessment assessment = this.assessmentService.getAssessment(params[3]);
+		context.put("assessment", assessment);
+
+		// get Assessment submissions
+		List submissions = this.submissionService.findAssessmentSubmissions(assessment, null, Boolean.TRUE, null, null);
+		context.put("submissions", submissions);
+
+		uiService.render(ui, context);
+	}
+
+	/**
+	 * Final initialization, once all dependencies are set.
+	 */
+	public void init()
+	{
+		super.init();
+		M_log.info("init()");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void post(HttpServletRequest req, HttpServletResponse res, Context context, String[] params) throws IOException
+	{
+		throw new IllegalArgumentException();
+	}
+
+	/**
+	 * @param assessmentService
+	 *        the assessmentService to set
+	 */
+	public void setAssessmentService(AssessmentService assessmentService)
+	{
+		this.assessmentService = assessmentService;
+	}
+
+	/**
+	 * @param submissionService
+	 *        the submissionService to set
+	 */
+	public void setSubmissionService(SubmissionService submissionService)
+	{
+		this.submissionService = submissionService;
+	}
+}
