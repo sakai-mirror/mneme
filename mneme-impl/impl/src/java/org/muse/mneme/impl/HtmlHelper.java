e6033e98d84ad856f6f198f2f63b8cbf395cca65
ggolden@umich.edu <ggolden@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-02-21 13:17:26 +0000

    htmlcleaner / jdom version we are not likely to use
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@45823 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- mneme-impl/impl/src/java/org/muse/mneme/impl/HtmlHelper.java
+++ mneme-impl/impl/src/java/org/muse/mneme/impl/HtmlHelper.java
@@ -21,11 +21,31 @@
 
 package org.muse.mneme.impl;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.htmlcleaner.HtmlCleaner;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.output.EscapeStrategy;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.jdom.xpath.XPath;
+
 /**
  * HtmlHelper has some utility methods for working with user entered HTML.
  */
 public class HtmlHelper
 {
+	/** Our logger. */
+	private static Log M_log = LogFactory.getLog(HtmlHelper.class);
+
 	/**
 	 * Clean some user entered HTML. Assures well formed XML. Assures all anchor tags have target=_blank.
 	 * 
@@ -35,6 +55,81 @@ public class HtmlHelper
 	 */
 	public static String clean(String source)
 	{
+		if (source == null) return null;
+
+		try
+		{
+			// parse possibly dirty html
+			HtmlCleaner cleaner = new HtmlCleaner(source);
+
+			//cleaner.setRecognizeUnicodeChars(true);
+			//cleaner.setAdvancedXmlEscape(true);
+			cleaner.setTranslateSpecialEntities(true);
+
+			// clean it up
+			cleaner.clean();
+
+			String pretty = cleaner.getPrettyXmlAsString();
+			// System.out.println(pretty);
+
+			// make a DOM for further processing
+			Document doc = cleaner.createJDom();
+
+			// assure target=_blank in all anchors
+			XPath x = XPath.newInstance("//a");
+			List l = x.selectNodes(doc);
+			for (Object o : l)
+			{
+				Element e = (Element) o;
+				e.setAttribute("target", "_blank");
+			}
+
+			// find the part we will save
+			Element rvElement = null;
+			XPath x2 = XPath.newInstance("/html/body");
+			l = x2.selectNodes(doc);
+			Element body = (Element) l.get(0);
+			if ((body.getContent().size() == 1) && (body.getChildren().size() == 1)
+					&& (((Element) (body.getChildren().get(0))).getName().equals("p")))
+			{
+				rvElement = (Element) body.getChildren().get(0);
+			}
+
+			// otherwise use the body, but change it to a paragraph
+			else
+			{
+				body.setName("p");
+				rvElement = body;
+			}
+
+			// write to a string
+			// Format format = Format.getRawFormat().setEscapeStrategy(new EscapeStrategy()
+			// {
+			// public boolean shouldEscape(char arg0)
+			// {
+			// return false;
+			// }
+			// });
+			XMLOutputter xmlOut = new XMLOutputter();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			xmlOut.output(rvElement, baos);
+			String rv = baos.toString("UTF-8");
+
+			return rv;
+		}
+		catch (IOException e)
+		{
+			M_log.warn(e);
+		}
+		catch (ParserConfigurationException e)
+		{
+			M_log.warn(e);
+		}
+		catch (JDOMException e)
+		{
+			M_log.warn(e);
+		}
+
 		return source;
 	}
 }
