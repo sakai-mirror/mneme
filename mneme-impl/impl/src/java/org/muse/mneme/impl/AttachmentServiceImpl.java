6436ec516b846d8263706756be75b85d1e232bcc
ggolden@umich.edu <ggolden@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-06-10 04:15:43 +0000

    MN-697 - disabled thumb creation in attachment service, made it configurable
    git-svn-id: https://source.sakaiproject.org/contrib/muse/mneme/trunk@50076 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- mneme-impl/impl/src/java/org/muse/mneme/impl/AttachmentServiceImpl.java
+++ mneme-impl/impl/src/java/org/muse/mneme/impl/AttachmentServiceImpl.java
@@ -117,6 +117,9 @@ public class AttachmentServiceImpl implements AttachmentService, EntityProducer
 	/** Dependency: IdManager. */
 	protected IdManager idManager = null;
 
+	/** Configuration: to make thumbs for images or not. */
+	protected boolean makeThumbs = false;
+
 	/** Dependency: SecurityService */
 	protected SecurityService securityService = null;
 
@@ -577,7 +580,7 @@ public class AttachmentServiceImpl implements AttachmentService, EntityProducer
 			// register as an entity producer
 			entityManager.registerEntityProducer(this, REFERENCE_ROOT);
 
-			M_log.info("init()");
+			M_log.info("init(): thumbs: " + this.makeThumbs);
 		}
 		catch (Throwable t)
 		{
@@ -703,6 +706,17 @@ public class AttachmentServiceImpl implements AttachmentService, EntityProducer
 	public void setIdManager(IdManager idManager)
 	{
 		this.idManager = idManager;
+	}
+
+	/**
+	 * Set the make thumbs setting
+	 * 
+	 * @param value
+	 *        the string of the boolean for the make thumbs setting.
+	 */
+	public void setMakeThumbs(String value)
+	{
+		this.makeThumbs = Boolean.valueOf(value);
 	}
 
 	/**
@@ -860,6 +874,9 @@ public class AttachmentServiceImpl implements AttachmentService, EntityProducer
 	 */
 	protected Reference addThumb(Reference resource, String name, byte[] body)
 	{
+		// if disabled
+		if (!this.makeThumbs) return null;
+
 		Reference ref = this.getReference(resource.getId());
 		String thumbId = ref.getId() + THUMB_SUFFIX;
 		String thumbName = name + THUMB_SUFFIX;
