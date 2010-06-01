60044175e26a9032f379e6e58ac5f867aa758576
ggolden@etudes.org <ggolden@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-06-01 17:43:50 +0000

    git-svn-id: https://source.sakaiproject.org/contrib/etudes/mneme/branches/MN-910@67931 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- mneme-impl/impl/src/java/org/etudes/mneme/impl/FillBlanksAnswerImpl.java
+++ mneme-impl/impl/src/java/org/etudes/mneme/impl/FillBlanksAnswerImpl.java
@@ -435,21 +435,6 @@ public class FillBlanksAnswerImpl implements TypeSpecificAnswer
 			return rv;
 		}
 
-		// we have answers
-		// if (this.answers.length != size)
-		// {
-		// M_log.warn("getEntryCorrects: answers length: " + this.answers.length + " != correct answers length: " + size + " question: "
-		// + this.answer.getQuestion().getId() + " submission: " + this.answer.getSubmission().getId());
-		//
-		// // return as if nothing was entered correctly, since we don't know what is missing
-		// for (int i = 0; i < size; i++)
-		// {
-		// rv.add(Boolean.FALSE);
-		// }
-		//
-		// return rv;
-		// }
-
 		for (int i = 0; i < size; i++)
 		{
 			// get this answer - it might be that the answers are shorter than the corrects, with some answers missing
