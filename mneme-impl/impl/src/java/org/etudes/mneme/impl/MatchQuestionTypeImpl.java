cf03663760b3ca5b18c42e88540194910aa38e64
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-12-08 22:05:03 +0000

    git-svn-id: https://source.sakaiproject.org/contrib/etudes/mneme/branches/mn-1166@77804 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ mneme-impl/impl/src/java/org/etudes/mneme/impl/MatchQuestionTypeImpl.java
@@ -0,0 +1,32 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/etudes/mneme/branches/mn-1166/mneme-impl/impl/src/java/org/etudes/mneme/impl/MatchQuestionTypeImpl.java $
+ * $Id: MatchQuestionTypeImpl.java 67735 2010-05-20 15:49:14Z ggolden@etudes.org $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2011 Etudes, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.etudes.mneme.impl;
+
+import org.etudes.mneme.api.MatchQuestion;
+
+/**
+ * MatchQuestionImpl handles questions for the true false question type.
+ */
+public class MatchQuestionTypeImpl extends QuestionImpl implements MatchQuestion
+{
+	
+}
